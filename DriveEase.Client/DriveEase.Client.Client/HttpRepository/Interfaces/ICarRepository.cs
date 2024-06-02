0e135492ed6cffe385eceae90235a24b7162444d
majkel1987 <97880836+majkel1987@users.noreply.github.com>
2024-06-02 08:37:06 +0200

    Repository cleanup

--- DriveEase.Client/DriveEase.Client.Client/HttpRepository/Interfaces/ICarRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using DriveEase.Application.Cars.Queries;
-namespace DriveEase.Client.HttpRepository.Interfaces
-{
-	public interface ICarRepository
-	{
-		Task<List<CarDto>> GetCars();
-	}
-}
--- DriveEase.Client/DriveEase.Client.Client/Program.cs
+++
@@ -1,9 +0,0 @@
-using DriveEase.Client;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-var uri = new Uri(builder.Configuration["ApiConfiguration:BaseAddress"] + "api/");
-
-builder.Services.AddClient(uri);
-
-await builder.Build().RunAsync();
